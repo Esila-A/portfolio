95cc59800d123050c528f9da0ec040c623f4d64a
Esila-A <esila.adajewa@gmail.com>
2022-07-26 17:51:26 +0200

    added footer and background parallax

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,23 @@
+import {FaLinkedinIn} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+
+let date = new Date();
+let year = date.getFullYear();
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <p className="footer_copyright">Copyright &copy; {year} Esila Adajewa</p>
+      <div className="footer_links">
+        <a href="https://github.com/Esila-A">
+          <FaGithub className="footer_links--icon"/>
+        </a>
+        <a href="https://www.linkedin.com/in/esila-adajewa-567296223/">
+          <FaLinkedinIn className="footer_links--icon"/>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
