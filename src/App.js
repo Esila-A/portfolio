ee902fea6bb9465a012dcf555540a4f805b1b43f
Esila-A <esila.adajewa@gmail.com>
2022-07-26 17:51:26 +0200

    added background parallax

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import { Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Header from "./components/header";
 import Work from "./components/work";
 import About from "./components/about";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
 
+
 function App() {
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Work />} />
-        <Route path="work" element={<Work />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Routes>
-      <Footer />
+      <div
+        className="app_background"
+        style={{ transform: `translateY(-${offsetY * 0.2}px)` }}
+      />
+      <div className="app_components">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Work />} />
+          <Route path="work" element={<Work />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
     </div>
   );
 }
